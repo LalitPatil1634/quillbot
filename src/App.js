3f8602a980e549b78e04a7db48f3f9ee6fad1002
lalit <lalitpatil1634@yahoo.com>
2023-01-06 21:39:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Cards } from './components/Cards';
+import { Details } from './components/Details';
+import { Header } from './components/Header';
+import { Sliders } from './components/Sliders';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='outerDiv'>
+      <Header />
+      <Sliders />
+      <Cards />
+      <Details />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="headerOuterDiv">
+      <div className="headerSection">
+        <p>Save time and write with confidence</p>
+        <div>
+          <button className="btn">Upgrade to QuillBot Premium</button>
+        </div>
+      </div>
+    </div>
+  );
+};
